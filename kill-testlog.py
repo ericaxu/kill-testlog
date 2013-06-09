7fd6e1de332da557b38be3d518fa0c24324fbcf4
Erica Xu <erica.xu@uwaterloo.ca>
2013-06-09 01:43:23 -0300

    Edit regexp to tolerate all whitespace

--- kill-testlog.py
+++ kill-testlog.py
@@ -2,8 +2,9 @@ import sublime, sublime_plugin
 
 class CleanlogCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		regions = self.view.find_all('^\s*console.log\(.*\);//t\n')
+		regexp = '^\s*c\s*o\s*n\s*s\s*o\s*l\s*e\s*.\s*l\s*o\s*g\s*\(.*\)\s*;\s*/\s*/\s*t\s*\n'
+		regions = self.view.find_all(regexp)
 		while regions :
 			print(regions[0])
 			self.view.erase(edit, regions[0])
-			regions = self.view.find_all('^\s*console.log\(.*\);//t\n')
+			regions = self.view.find_all(regexp)
