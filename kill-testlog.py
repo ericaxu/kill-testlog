cfdf1252d4d287e7c2f8356aee8a9592ab9e5fa1
Erica Xu <erica.xu@uwaterloo.ca>
2013-06-09 01:32:05 -0300

    Finished it

--- kill-testlog.py
+++ kill-testlog.py
@@ -1,5 +1,9 @@
 import sublime, sublime_plugin
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class CleanlogCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		regions = self.view.find_all('^\s*console.log\(.*\);//t\n')
+		while regions :
+			print(regions[0])
+			self.view.erase(edit, regions[0])
+			regions = self.view.find_all('^\s*console.log\(.*\);//t\n')
